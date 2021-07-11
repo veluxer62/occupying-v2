a0817d9932acbf4926557a7885c856a373939830
남경호 <veluxer62@gmail.com>
2021-07-11 16:41:18 +0900

    feat: 코레일 클라이언트 추가

--- src/main/kotlin/com/veluxer/occupying/application/AppConfig.kt
+++ src/main/kotlin/com/veluxer/occupying/application/AppConfig.kt
@@ -1,8 +1,27 @@
 package com.veluxer.occupying.application
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
 
 @Configuration
 @EnableConfigurationProperties(KorailProperties::class)
-class AppConfig
+class AppConfig {
+
+    @Bean
+    fun korailClient(korailProperties: KorailProperties): WebClient {
+        return WebClient.builder()
+            .baseUrl(korailProperties.host)
+            .defaultUriVariables(mapOf("Device" to "AD"))
+            .clientConnector(
+                ReactorClientHttpConnector(
+                    HttpClient.create().responseTimeout(Duration.ofMillis(korailProperties.timeout))
+                )
+            )
+            .build()
+    }
+}
