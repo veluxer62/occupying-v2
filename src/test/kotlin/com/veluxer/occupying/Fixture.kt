a802b6556d3796272516b27ade04a6cb12fae34d
남경호 <veluxer62@gmail.com>
2021-07-24 15:27:43 +0900

    test: 코레일 열차 Fixture 추가

--- src/test/kotlin/com/veluxer/occupying/Fixture.kt
+++ src/test/kotlin/com/veluxer/occupying/Fixture.kt
@@ -1,6 +1,11 @@
 package com.veluxer.occupying
 
+import com.veluxer.occupying.domain.SeatStatus
 import com.veluxer.occupying.domain.Station
+import com.veluxer.occupying.domain.TrainType
+import com.veluxer.occupying.domain.korail.KorailConstraint.DATE_FORMAT
+import com.veluxer.occupying.domain.korail.KorailConstraint.TIME_FORMAT
+import com.veluxer.occupying.domain.korail.KorailTrain
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
@@ -15,4 +20,16 @@ internal object Fixture {
     val SEARCH_DEPARTURE_STATION = Station.SEOUL
     val SEARCH_DESTINATION_STATION = Station.BUSAN
     const val TRAIN_NO = "009"
+    val RESERVATION_KORAIL_TRAIN = KorailTrain(
+        number = TRAIN_NO,
+        trainType = TrainType.KTX.code,
+        seatStatus = SeatStatus.AVAILABLE.code.first(),
+        fare = "00000000059800",
+        departureDate = SEARCH_DEPARTURE_DATETIME.toLocalDate().format(DATE_FORMAT),
+        departureTime = SEARCH_DEPARTURE_DATETIME.toLocalTime().format(TIME_FORMAT),
+        departureStation = SEARCH_DEPARTURE_STATION.code,
+        arrivalDate = SEARCH_DEPARTURE_DATETIME.toLocalDate().format(DATE_FORMAT),
+        arrivalTime = "093800",
+        destinationStation = SEARCH_DESTINATION_STATION.code
+    )
 }
